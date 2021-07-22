a32e3fc46b1f85506e8403690651711a9d83936e
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 17:52:35 -0500

    Overriding: super

--- TestDog.java
+++ TestDog.java
@@ -6,21 +6,15 @@ class Animal {
  
  class Dog extends Animal {
     public void move() {
+       super.move();   // invokes the super class method
        System.out.println("Dogs can walk and run");
-    }
-    public void bark() {
-       System.out.println("Dogs can bark");
     }
  }
  
  public class TestDog {
  
     public static void main(String args[]) {
-       Animal a = new Animal();   // Animal reference and object
        Animal b = new Dog();   // Animal reference but Dog object
- 
-       a.move();   // runs the method in Animal class
        b.move();   // runs the method in Dog class
-       b.bark();
     }
  }
